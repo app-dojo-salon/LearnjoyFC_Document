68370c590776664cf6c236e11f5654a15b6b5953
koji-torishima <torikou3685@gmail.com>
2020-08-29 11:35:40 +0900

    やることの追加

--- sample.swift
+++ sample.swift
@@ -1,5 +1,28 @@
-
+// 作業練習用ファイル
 var name: String = "太郎"
-
 // MARK:- Torishima Commit
 var age: Int = 21
+
+
+
+
+
+
+// ↑↑↑↑↑↑↑↑↑↑上を編集して下の項目をやってみてください↓↓↓↓↓↓↓↓↓↓↓
+// やること
+/*
+ - コミット
+ - プッシュ
+ - PR作成
+ - PRレビュー
+ - PR approve
+ - PRマージ
+ - プル
+ 
+ ** 資料 **
+ - https://note.com/echa/n/nbc3fd0ba833a
+ - https://qiita.com/ko-he-8/items/94e872f2154829c868df
+ Git関連で資料があったら貼ってOKです
+ */
+
+
